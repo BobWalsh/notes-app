3ee93786ea83aa386da0c476a3ade9dd7ac506cc
Bob Walsh <bob.walsh@47hats.com>
2020-09-03 13:33:57 -0700

    found typo on params:[:page_id]

--- app/reflexes/checklist_item_reflex.rb
+++ app/reflexes/checklist_item_reflex.rb
@@ -2,6 +2,23 @@
 
 class ChecklistItemReflex < ApplicationReflex
 
+  def create
+    notebook = current_user.notebooks.find(params[:notebook_id])
+    page = notebook.pages.find(params[:page_id])
+    checklist = page.checklists.find(params[:checklist_id])
+    checklist_item = checklist.checklist_items.new(checklist_items_params)
+    checklist_item.status = 'not-started'
+    checklist_item.save
+
+    # target_id = "#checklist-items-for-#{checklist.id}"
+    # morph target_id, ApplicationController.render(
+    #   partial: "checklists/items", locals:{
+    #     notebook:notebook,
+    #     page: page,
+    #     checklist: checklist
+    #     }
+    #   )
+  end
 
   def change_status
     checklist_item = ChecklistItem.includes(:checklist).find(element.dataset.id)
@@ -11,5 +28,12 @@ class ChecklistItemReflex < ApplicationReflex
       locals: { checklist: checklist_item.checklist }
     )
   end
+
+  private
+
+  def checklist_items_params
+    params.required(:checklist_item).permit(:checklist_id, :title, :status)
+  end
+
 
 end
